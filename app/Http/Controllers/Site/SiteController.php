0260d01ea16ab702e62b84385a4860ce070f25cb
nmd <you@example.com>
2016-11-21 01:11:08 +0700

    fix

--- app/Http/Controllers/Site/SiteController.php
+++ app/Http/Controllers/Site/SiteController.php
@@ -392,7 +392,6 @@ class SiteController extends Controller
             ->where('status', ACTIVE)
             ->where('start_date', '<=', date('Y-m-d H:i:s'))
             ->where('slug', 'like', '%'.$slug.'%')
-            // ->orWhere('name', 'like', '%'.$request->name.'%')
             ->whereNull('deleted_at')
             ->orderBy('start_date', 'desc')
             ->paginate(PAGINATE);
