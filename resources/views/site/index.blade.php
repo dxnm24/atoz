aaa6a3405e06187948c8db7681db09385ffed6f2
nmd <you@example.com>
2016-11-25 20:02:31 +0700

    fix

--- resources/views/site/index.blade.php
+++ resources/views/site/index.blade.php
@@ -49,27 +49,27 @@
 			<div class="box">
 				<ul class="box-tabs clearfix" data-tabs id="box-tabs-{{ $value->id }}">
 					<li class="tabs-title is-active">
-						<h3><a href="#{{ 'latest-'.$value->slug }}" aria-selected="true"><i class="fa fa-clock-o" aria-hidden="true"></i> Latest {!! $value->name !!}</a></h3>
+						<h3><a href="#{{ 'latest-'.$value->slug }}" aria-selected="true" rel="nofollow"><i class="fa fa-clock-o" aria-hidden="true"></i> Latest {!! $value->name !!}</a></h3>
 					</li>
 					<li class="tabs-title">
-						<h3><a href="#{{ 'best-'.$value->slug }}"><i class="fa fa-star" aria-hidden="true"></i> Best {!! $value->name !!}</a></h3>
+						<h3><a href="#{{ 'best-'.$value->slug }}" rel="nofollow"><i class="fa fa-star" aria-hidden="true"></i> Best {!! $value->name !!}</a></h3>
 					</li>
 				</ul>
 				<div class="box-inner" data-tabs-content="box-tabs-{{ $value->id }}">
 					<div class="tabs-panel is-active" id="{{ 'latest-'.$value->slug }}">
-						<a href="{{ $urlLatest }}" class="btn-seemore float-right hide-for-small-only"><span>See More</span></a>
+						<a href="{{ $urlLatest }}" class="btn-seemore float-right hide-for-small-only" rel="nofollow"><span>See More</span></a>
 						<div class="clearfix"></div>
 						@include('site.game.box', array('data' => $value->games))
 					</div>
 					<div class="tabs-panel" id="{{ 'best-'.$value->slug }}">
-						<a href="{{ $urlBest }}" class="btn-seemore float-right hide-for-small-only"><span>See More</span></a>
+						<a href="{{ $urlBest }}" class="btn-seemore float-right hide-for-small-only" rel="nofollow"><span>See More</span></a>
 						<div class="clearfix"></div>
 						@include('site.game.box', array('data' => $value->games2))
 					</div>
 				</div>
 				<div class="row column show-for-small-only box-seemore">
-					<a href="{{ $urlLatest }}" class="btn-seemore">See More Latest {!! $value->name !!}<i class="fa fa-arrow-circle-o-right" aria-hidden="true"></i></a>
-					<a href="{{ $urlBest }}" class="btn-seemore">See More Best {!! $value->name !!}<i class="fa fa-arrow-circle-o-right" aria-hidden="true"></i></a>
+					<a href="{{ $urlLatest }}" class="btn-seemore" rel="nofollow">See More Latest {!! $value->name !!}<i class="fa fa-arrow-circle-o-right" aria-hidden="true"></i></a>
+					<a href="{{ $urlBest }}" class="btn-seemore" rel="nofollow">See More Best {!! $value->name !!}<i class="fa fa-arrow-circle-o-right" aria-hidden="true"></i></a>
 					<a href="{{ $url }}" class="btn-seemore">See More<i class="fa fa-arrow-circle-o-right" aria-hidden="true"></i></a>
 				</div>
 			</div>
